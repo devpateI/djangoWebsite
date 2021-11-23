b4082b3cca399d2c2d089d0c158068d0f7421c0e
Dev Patel <dev.dp721@gmail.com>
2021-11-23 16:18:36 +0530

    Contect Us done

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib import admin
 from home import views
 import sys
+
+admin.site.site_header = "Techshop Admin"
+admin.site.index_title = "Welcome to Techshop"
+admin.site.site_title = "Techshop"
 urlpatterns = [
-    path('', views.index,name='home'),
+    path('home', views.index,name='home'),
     path('about',views.about,name = 'about'),
     path('contact',views.contact,name = 'contact'),
     path('services',views.services,name='services')
--- home/views.py
+++ home/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from home.models import Contact
 # Create your views here.
 def index(request):
-    context = {
-        'variable':'this is sent'
-    }
-    return render(request,'index.html',context)
+    return render(request,'index.html')
 
 def about(request):
-    return HttpResponse("this is about page")
+    return render(request,'about.html')
 
 def contact(request):
-    return HttpResponse("this is contact page")
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date = datetime.today())
+        contact.save()
+    return render(request,'contact.html')
 
 def services(request):
-    return HttpResponse("this is services page")
+    return render(request,'services.html')
 
 
